d2e645af9ebd3164b5016370dab0e7e6a68aed25
Jens Fischer <web@jens-fischer.eu>
2019-10-15 14:15:36 +0200

    Make the window size match the tile size

--- config/config.exs
+++ config/config.exs
@@ -5,7 +5,7 @@ use Mix.Config
 # Configure the main viewport for the Scenic application
 config :snake, :viewport, %{
   name: :main_viewport,
-  size: {700, 600},
+  size: {672, 576},
   default_scene: {Snake.Scene.Game, nil},
   drivers: [
     %{
